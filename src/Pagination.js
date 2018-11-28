0b8938f2e465071fd34f1d511498ab458f769a1d
Diana Nemyrovska <diana.nemyrovska@gmail.com>
2018-11-28 21:30:22 +0700

    Added basic logic

--- src/Pagination.js
+++ src/Pagination.js
@@ -1,14 +1,46 @@
 import React, { Component } from 'react';
-import './App.css';
+import './Pagination.css';
+
+const LIST = ["a", "b", "c", "d", "e", "f", "g", "k", "l",];
+const ITEMS_PER_PAGE = 3;
 
 class Pagination extends Component {
-  render() {
-    return (
-      <div className="App">
+  state = {
+    activePage: 1
+  }
 
+  setActivePage = (page) => () => {
+    console.log(page);
+    this.setState({
+      activePage: page
+    })
+  }
+  render() {
+    const lastItemIndex = this.state.activePage * ITEMS_PER_PAGE;
+    const firstItemIndex = lastItemIndex - ITEMS_PER_PAGE;
+    const currentItems = LIST.slice(firstItemIndex, lastItemIndex);
+    const rangeOfPages = Math.ceil(LIST.length / ITEMS_PER_PAGE);
+    const pageNumbers = [];
+    for (let i = 1; i <= rangeOfPages; i++) {
+      pageNumbers.push(<li
+        className="page-number"
+        key={i}
+        onClick={this.setActivePage(i)}
+      >
+        {i}
+      </li>)
+    }
+    return (
+      <div className="App" >
+        <ul className="list">
+          {currentItems.map((item) => <li key={item}>{item}</li>)}
+        </ul>
+        <ul className="pages">
+          {pageNumbers}
+        </ul>
       </div>
     );
   }
 }
 
-export default App;
+export default Pagination;
